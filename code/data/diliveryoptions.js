7edefa3d2e96a9c1e52ba3c520bac2a9a7fe00e2
Nav1n <155826407+Nav1n-5@users.noreply.github.com>
2024-06-05 15:55:13 +0545

    save the data and generated HTML for dilivery options

---
+++ code/data/diliveryoptions.js
@@ -0,0 +1,18 @@
+ export const deliveryOptions =[{
+  id:'1',
+  diliveryDays:7,
+  priceCents:0
+},
+{
+  id:2,
+  diliveryDays:3,
+  priceCents:499,
+
+},
+{
+  id:3,
+  diliveryDays:1,
+  priceCents:999
+}
+
+];
